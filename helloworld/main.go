6b9361d8a5a1036b592321882ff0e0b648bc2289
Alicja Caban <alicja.caban@nokia.com>
2018-01-16 13:54:48 +0100

    add Prometheus to monitoring service

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-"encoding/json"
-"github.com/gorilla/mux"
-"log"
-"net/http"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 func GetHelloEndpoint(w http.ResponseWriter, r *http.Request) {
@@ -12,8 +13,17 @@ func GetHelloEndpoint(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(params)
 }
 
+var responseMetric = prometheus.NewHistogram(
+	prometheus.HistogramOpts{
+		Name: "request_duration_milliseconds",
+		Help: "Request latency distribution",
+		Buckets: prometheus.ExponentialBuckets(10.0, 1.13, 40),
+	})
+
 func main() {
+	prometheus.MustRegister(responseMetric)
 	router := mux.NewRouter()
+	router.Handle("/metrics", prometheus.Handler())
 	router.HandleFunc("/hello", GetHelloEndpoint).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
